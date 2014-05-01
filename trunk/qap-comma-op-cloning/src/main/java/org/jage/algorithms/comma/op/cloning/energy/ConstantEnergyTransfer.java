adf481f3b17710bb84431a2ebc1cd2e1478fb96e
norbert.tusinski@gmail.com <norbert.tusinski@gmail.com@3d755c5b-8ce6-902b-a3c2-12dd8f4e70e0>
2014-05-01 00:44:22 +0000

    comma op with cloning take 1

---
+++ trunk/qap-comma-op-cloning/src/main/java/org/jage/algorithms/comma/op/cloning/energy/ConstantEnergyTransfer.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (C) 2006 - 2012
+ *   Pawel Kedzior
+ *   Tomasz Kmiecik
+ *   Kamil Pietak
+ *   Krzysztof Sikora
+ *   Adam Wos
+ *   Lukasz Faber
+ *   Daniel Krzywicki
+ *   and other students of AGH University of Science and Technology.
+ *
+ * This file is part of AgE.
+ *
+ * AgE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AgE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AgE.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ * Created: 2012-01-30
+ * $Id: ConstantEnergyTransfer.java 471 2012-10-30 11:17:00Z faber $
+ */
+
+package org.jage.algorithms.comma.op.cloning.energy;
+
+import org.jage.emas.agent.IndividualAgent;
+import org.jage.emas.energy.EnergyTransfer;
+
+/**
+ * A constant energy transfer strategy. Always transfers the same fixed amount of energy. However, if the source have
+ * less energy left, only this much will be transferred.
+ *
+ * @author AGH AgE Team
+ */
+public class ConstantEnergyTransfer implements EnergyTransfer<IndividualAgent>
+{
+
+   private double transferredEnergy;
+
+   public void setTransferredEnergy (final double transferredEnergy)
+   {
+      this.transferredEnergy = transferredEnergy;
+   }
+
+   @Override
+   public final double transferEnergy (final IndividualAgent source, final IndividualAgent target)
+   {
+      double f1 = source.getEffectiveFitness();
+      double f2 = target.getEffectiveFitness();
+
+      double e1 = source.getEnergy();
+      double e2 = target.getEnergy();
+
+      if (f1 * e1 > f2 * e2)
+      {
+         source.changeEnergyBy(transferredEnergy);
+         target.changeEnergyBy(-transferredEnergy);
+         return -transferredEnergy;
+      }
+      else
+      {
+         source.changeEnergyBy(-transferredEnergy);
+         target.changeEnergyBy(transferredEnergy);
+         return transferredEnergy;
+      }
+   }
+}
---
+++ trunk/qap-comma-op-cloning/src/test/java/Test.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (C) 2006 - 2012
+ *   Pawel Kedzior
+ *   Tomasz Kmiecik
+ *   Kamil Pietak
+ *   Krzysztof Sikora
+ *   Adam Wos
+ *   Lukasz Faber
+ *   Daniel Krzywicki
+ *   and other students of AGH University of Science and Technology.
+ *
+ * This file is part of AgE.
+ *
+ * AgE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AgE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AgE.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ * Created: 2011-12-16
+ * $Id: InitializationActionStrategy.java 471 2012-10-30 11:17:00Z faber $
+ */
+
+import org.jage.algorithms.comma.op.cloning.evaluate.Evaluator;
+import org.jage.algorithms.comma.op.cloning.input.InputDataHolder;
+import org.jage.solution.IVectorSolution;
+import org.jage.solution.VectorSolution;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * User: Norbert Tusiński
+ * Date: 11/27/13
+ * Time: 4:00 PM
+ */
+public class Test {
+    private static final Logger LOGGER = Logger.getLogger(Test.class.getName());
+
+    public static void main(String[] args) throws IOException {
+        //int[] tab = new int[]{7, 0, 5, 1, 10, 9, 2, 4, 8, 6, 11, 3};
+        int[] tab = new int[]{1, 0, 3, 2};
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < tab.length; i++) {
+            list.add(tab[i]);
+        }
+        System.out.println(new File(".").getCanonicalPath());
+        InputDataHolder.getInstance("qap-comma-op/input/example");
+        IVectorSolution<Integer> sol = new VectorSolution<Integer>(list);
+        LOGGER.info("Result is: " + new Evaluator().evaluate(sol));
+    }
+}
