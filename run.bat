0e4850a815d0f8eb1aea8381bfb48684e6401ac0
Norbert Tusiński <norbert.tusinski@gmail.com>
2015-07-28 18:35:15 +0200

    One bat to rule them all!

---
+++ run.bat
@@ -0,0 +1,15 @@
+cd qap-comma-op
+call run.bat
+cd ..
+
+cd qap-comma-op-cloning
+call run.bat
+cd ..
+
+cd qap-comma-op-double-mutation
+call run.bat
+cd ..
+
+cd qap-comma-op-islands
+call run.bat
+cd ..
